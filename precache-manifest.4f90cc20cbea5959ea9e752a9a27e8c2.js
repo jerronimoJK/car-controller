7ca0377200bc4573e242470fb139a7eccb03c7db
Jerry <jaroslawkuliga@gmail.com>
2019-01-20 14:57:14 +0100

    Updates

---
+++ precache-manifest.4f90cc20cbea5959ea9e752a9a27e8c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/car-controller/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4623bc1dbb143ae7162e",
+    "url": "/car-controller/static/js/runtime~main.4623bc1d.js"
+  },
+  {
+    "revision": "13b924d4a58ba6ea178b",
+    "url": "/car-controller/static/js/main.13b924d4.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/car-controller/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "13b924d4a58ba6ea178b",
+    "url": "/car-controller/static/css/main.3cc15b9a.chunk.css"
+  },
+  {
+    "revision": "820095674efd849be43540e50cd0d037",
+    "url": "/car-controller/index.html"
+  }
+];
